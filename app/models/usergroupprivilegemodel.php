afe3b39e8be0aba458637af8530bfa7b17c7f225
Abdallah-SE <abdalla.a911@yahoo.com>
2021-08-11 19:39:26 +0300

    return the privileges of the login user

--- app/models/usergroupprivilegemodel.php
+++ app/models/usergroupprivilegemodel.php
@@ -16,5 +16,18 @@ class UserGroupPrivilegeModel extends AbstractModel{
     public function getTableName(){
         return self::$tableName;
     }
-
+    
+    /// get the url from the privilege
+    public static function getURLGroupPrivileges($groupId){
+        $sql = 'SELECT augp.*, aup.Privilege FROM '. self::$tableName .' augp INNER JOIN app_users_privileges aup ON aup.PrivilegeID = augp.PrivilegeID';
+        $sql .= ' WHERE augp.GroupID = '.$groupId;
+        $prifileges = self::get($sql);
+        $arr_privileges = [];
+        if(FALSE !== $prifileges){
+            foreach ($prifileges as $prifilege_value) {
+                $arr_privileges[] = $prifilege_value->Privilege;
+            }
+        }
+        return $arr_privileges;
+    }
 }
