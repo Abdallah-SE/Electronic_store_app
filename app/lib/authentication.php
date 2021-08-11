751258f88a0fb143851c1c8a76c2d5f32f39a7fc
Abdallah-SE <abdalla.a911@yahoo.com>
2021-08-11 19:23:09 +0300

    class to manage the accounts and their privileges

--- app/lib/authentication.php
+++ app/lib/authentication.php
@@ -5,6 +5,15 @@ class Authentication {
     private static $_instance;
     private  $_session;
     
+    /// url already avaiable for all user
+    private $_defaultRoutes = [
+        '/index/default',
+        '/authorizing/logout',
+        '/users/default',
+        '/users/changepasswords',
+        '/language/default',
+        '/accessdenied/default'
+    ];
     private function __construct($session) {
         $this->_session = $session;
     }  
@@ -18,5 +27,11 @@ class Authentication {
     }
     public function checkAuthorized(){
         return (isset($this->_session->user));
+    }
+    public function checkForAccess($controller, $action){
+        $url = strtolower('/'. $controller .'/' .$action);
+        if(in_array($url, $this->_defaultRoutes) || in_array($url, $this->_session->user->privileges)){
+            return TRUE;
+        }
     }
 }
