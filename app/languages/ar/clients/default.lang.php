f99db469329f10250fd95938bea7b17c1be898af
Abdallah-SE <abdalla.a911@yahoo.com>
2021-08-14 02:20:25 +0300

    Arabic text for client's info displayed in the  view of the client

---
+++ app/languages/ar/clients/default.lang.php
@@ -0,0 +1,13 @@
+<?php
+$_['title'] = 'العملاء';
+$_['text_header'] = 'العملاء';
+$_['client_title'] = 'العملاء';
+$_['add_new_client'] = 'أضافة عميل جديد';
+$_['text_client_name'] = 'اسم العميل';
+$_['text_client_phone'] = 'رقم الهاتف';
+$_['text_client_email'] = 'عنوان البريد الالكتروني';
+$_['text_client_address'] = 'عنوان العميل';
+$_['text_client_table_actions'] = 'لوحة التحكم';
+$_['text_delete_confirm'] = 'هل حقا تريد حذف هذا العميل';
+$_[''] = '';
+  
---
+++ app/languages/ar/clients/messages.lang.php
@@ -0,0 +1,8 @@
+<?php
+$_['message_create_success'] = 'تم تسجيل العميل بنجاح';
+$_['message_create_failed'] = 'خطأ في تسجيل العميل';
+$_['message_delete_success'] = 'تم حذف العميل بنجاح';
+$_['message_delete_failed'] = 'خطأ اثناء حذف العميل';
+$_['message_user_exists_failed'] = 'خطأ اسم العميل موجود بالفعل';
+$_['message_email_exists_failed'] = 'خطأ بريد العميل الالكتروني  موجود بالفعل';
+  
