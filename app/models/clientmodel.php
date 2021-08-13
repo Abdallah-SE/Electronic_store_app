6d481110efc9b2274ee8039c01eb1b59e41a65fb
Abdallah-SE <abdalla.a911@yahoo.com>
2021-08-14 02:22:07 +0300

    model ( business logic and application data) for the client table

---
+++ app/models/clientmodel.php
@@ -0,0 +1,21 @@
+<?php
+namespace PHPMVC\Models;
+class ClientModel extends AbstractModel{
+    
+    public $ClientID;
+    public $Name;
+    public $PhoneNumber;
+    public $Email;
+    public $Address;
+    
+    public static $tableName = 'app_clients';
+    
+    public static $tableSchema = array(
+        'Name'                   =>self::DATA_TYPE_STR,
+        'PhoneNumber'            =>self::DATA_TYPE_STR,
+        'Email'                  =>self::DATA_TYPE_STR,
+        'Address'                =>self::DATA_TYPE_STR
+    );
+    protected static $primaryKey = 'ClientID';
+
+}
