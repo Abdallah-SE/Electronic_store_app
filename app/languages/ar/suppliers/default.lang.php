ea98fc192ab139e5e4375d8fd093bf0e3d69560b
Abdallah-SE <abdalla.a911@yahoo.com>
2021-08-14 01:42:24 +0300

    Arabic text for supplier's info displayed in the suppliers view

---
+++ app/languages/ar/suppliers/default.lang.php
@@ -0,0 +1,13 @@
+<?php
+$_['title'] = 'الموردين';
+$_['text_header'] = 'الموردين';
+$_['supplier_title'] = 'الموردين';
+$_['add_new_supplier'] = 'أضافة مورد جديد';
+$_['text_supplier_name'] = 'اسم المورد';
+$_['text_supplier_phone'] = 'رقم الهاتف';
+$_['text_supplier_email'] = 'عنوان البريد الالكتروني';
+$_['text_supplier_address'] = 'عنوان المورد';
+$_['text_supplier_table_actions'] = 'لوحة التحكم';
+$_['text_delete_confirm'] = 'هل حقا تريد حذف هذا المورد';
+$_[''] = '';
+  
---
+++ app/languages/ar/suppliers/messages.lang.php
@@ -0,0 +1,8 @@
+<?php
+$_['message_create_success'] = 'تم تسجيل المورد بنجاح';
+$_['message_create_failed'] = 'خطأ في تسجيل المورد';
+$_['message_delete_success'] = 'تم حذف المورد بنجاح';
+$_['message_delete_failed'] = 'خطأ اثناء حذف المورد';
+$_['message_user_exists_failed'] = 'خطأ اسم المورد موجود بالفعل';
+$_['message_email_exists_failed'] = 'خطأ بريد المورد الالكتروني  موجود بالفعل';
+  
