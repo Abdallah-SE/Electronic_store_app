23e8b8469440f854f3b508a599c3cb438b2f5fa2
Abdallah-SE <abdalla.a911@yahoo.com>
2021-08-14 16:52:07 +0300

    model of business logic and application data   interact with product category table

---
+++ app/models/productcategorymodel.php
@@ -0,0 +1,18 @@
+<?php
+namespace PHPMVC\Models;
+class SProductCategoryModel extends AbstractModel{
+    
+    public $CategoryID;
+    public $Name;
+    public $Image;
+    
+    public static $tableName = 'app_prodcuts_categories';
+    
+    public static $tableSchema = array(
+        'Name'       =>self::DATA_TYPE_STR,
+        'Image'      =>self::DATA_TYPE_STR
+            
+    );
+    protected static $primaryKey = 'CategoryID';
+
+}
