a979460c669a67f063c9245a0b88ae7bbf873c42
Abdallah-SE <abdalla.a911@yahoo.com>
2021-08-17 07:19:59 +0300

    add some mechanisms best practice for add, edit, view and remove category

--- app/controllers/categoriescontroller.php
+++ app/controllers/categoriescontroller.php
@@ -10,7 +10,7 @@ use PHPMVC\LIB\Helper;
 use PHPMVC\lib\Messenger;
 class CategoriesController extends AbstractController {
     private $_createRules = [
-        'Name'         => 'req|alphanum|between(3,30)'
+        'Name'         => 'req|alphanum|between(3,35)'
         ];  
 
     use InputFilter;
@@ -29,12 +29,22 @@ class CategoriesController extends AbstractController {
         $this->language->load('categories.labels');
         $this->language->load('validation.errors');
         $this->language->load('categories.messages');
+        $uploadError = FALSE;
         // TODO search for good solution for secure and check file type
         if(isset($_POST['submit']) && $this->isValidInput($this->_createRules, $_POST)){
             $category = new CategoryModel();
             $category->Name = $this->filterStr($_POST['Name']);
-            $category->Image = (new UploadHandler($_FILES['Image']))->uploadFile()->getFile();
-            if($category->save()){
+            if(!empty($_FILES['Image']['name'])){
+                $upload_ob = new UploadHandler($_FILES['Image']);
+                try {
+                    $upload_ob->uploadFile();
+                    $category->Image = $upload_ob->getFile();
+                } catch (\Exception $exc) {
+                    $this->messeger->add($exc->getMessage(), Messenger::ERROR_MESSEEGE);
+                    $uploadError = TRUE;
+                }
+                        }
+            if($uploadError === FALSE && $category->save()){
                 $this->messeger->add($this->language->get('message_create_success'));
                 $this->redirect('/categories');
             }  else {
@@ -46,23 +56,37 @@ class CategoriesController extends AbstractController {
     // edit an existing user data
     public function editAction(){
         $this->language->load('template.common');
-        $this->language->load('users.edit');
-        $this->language->load('users.labels');
+        $this->language->load('categories.edit');
+        $this->language->load('categories.labels');
         $this->language->load('validation.errors');
-        $this->language->load('users.messages');
-        // get  the id of selected user to edit 
+        $this->language->load('categories.messages');
+        $uploadError = FALSE;
+        // get  the id of selected category to edit 
         $id = $this->filterInt($this->_params[0]);
         $category = CategoryModel::getByPK($id);
-        // check for if the user exists or trying the user who already login to edit himself
-        if($category === FALSE || $this->session->user->UserID == $id){
-            $this->redirect('/users');
+        // check for if the category exists or trying the user who already login to edit himself
+        if($category === FALSE){
+            $this->redirect('/categories');
         }
-        $this->_data['user'] = $category;
-        $this->_data['groups'] = UserGroupModel::getAll();
-        if(isset($_POST['submit']) && $this->isValidInput($this->_editRules, $_POST)){
-            $category->PhoneNumber = $this->filterStr($_POST['phone']);
-            $category->GroupID = $this->filterStr($_POST['GroupID']);
-            if($category->save()){
+        $this->_data['category'] = $category;
+        if(isset($_POST['submit']) && $this->isValidInput($this->_createRules, $_POST)){
+            $category->Name = $this->filterStr($_POST['Name']);
+            // check if the image is ste or not or it's empty
+            if((!empty($_FILES['Image']['name']))){
+                // remove the current image
+                if($category->Image !=='' && (file_exists(UPLOAD_MEMORY_IMG. DS .$category->Image)) && is_writable(UPLOAD_MEMORY_IMG)){
+                    unlink(UPLOAD_MEMORY_IMG. DS .$category->Image);
+                }
+                $upload_ob = new UploadHandler($_FILES['Image']);
+                try {
+                    $upload_ob->uploadFile();
+                    $category->Image = $upload_ob->getFile();
+                } catch (\Exception $exc) {
+                    $this->messeger->add($exc->getMessage(), Messenger::ERROR_MESSEEGE);
+                    $uploadError = TRUE;
+                }
+            }
+            if($uploadError === FALSE && $category->save()){
                 $this->messeger->add($this->language->get('message_create_success'));
                 $this->redirect('/categories');
             }  else {
@@ -74,33 +98,24 @@ class CategoriesController extends AbstractController {
     // selected user to be deleted
     public function deleteAction(){
         $this->language->load('categories.messages');
-        // get  the id of selected user to edit 
+        // get  the id of selected category to edit 
         $id = $this->filterInt($this->_params[0]);
-        $user = UserModel::getByPK($id);
+        $category = CategoryModel::getByPK($id);
         
-        if($user === FALSE || $this->session->user->UserID == $id){
+        if($category === FALSE){
         $this->messeger->add($this->language->get('message_delete_failed'), Messenger::ERROR_MESSEEGE);
         $this->redirect('/categories');
         }
-        $this->language->load('users.messages');
-        if($user->delete()){
+        if($category->delete()){
+            // remove the current image
+            if($category->Image !=='' && (file_exists(UPLOAD_MEMORY_IMG. DS .$category->Image)) && is_writable(UPLOAD_MEMORY_IMG)){
+                unlink(UPLOAD_MEMORY_IMG.DS.$category->Image);
+            }
             $this->messeger->add($this->language->get('message_delete_success'));
             $this->redirect('/categories');
         }else {
             $this->messeger->add($this->language->get('message_delete_failed'), Messenger::ERROR_MESSEEGE);
-            $this->redirect('/categories');
         }
+        $this->redirect('/categories');
     }  
-   
-    public function userExistsAjaxAction(){
-        if(isset($_POST['userName'])){
-            header('Content-type: text/plain');
-            if(UserModel::userExists($this->filterStr($_POST['userName'])) !== false){
-                echo 1;
-            }  else {
-                echo 0;
-            }
-        }
-    }
-    
 }
