87abb151d41f0828ff9d3754ddcb1390ed0e7239
Abdallah-SE <abdalla.a911@yahoo.com>
2021-08-19 04:43:53 +0300

    modify the product model in productmodel file

--- app/models/productmodel.php
+++ app/models/productmodel.php
@@ -1,29 +1,36 @@
 <?php
 namespace PHPMVC\Models;
 class ProductModel extends AbstractModel{
-    public $UserID;
-    public $Username;
-    public $Password;
-    public $Email;
-    public $PhoneNumber;
-    public $SubscriptionDate;
-    public $LastLogin;
-    public $GroupID;
-    public $Status;
-    
+    public $CategoryID;
+    public $Name;
+    public $Image;
+    public $Quantity;
+    public $BuyPrice;
+    public $BarCode;
+    public $Unit;
+    public $SellPrice;
+   
     public static $tableName = 'app_products_list';
+    protected static $primaryKey = 'ProductID';
+    
     public static $tableSchema = array(
         'CategoryID'       =>self::DATA_TYPE_INT,
         'Name'             =>self::DATA_TYPE_STR,
         'Image'            =>self::DATA_TYPE_STR,
         'Quantity'         =>self::DATA_TYPE_INT,
-        'Price'            =>self::DATA_TYPE_DECIMAL,
+        'BuyPrice'            =>self::DATA_TYPE_DECIMAL,
         'BarCode'          =>self::DATA_TYPE_STR,
-        'Unit'             =>self::DATA_TYPE_INT
+        'Unit'             =>self::DATA_TYPE_INT,
+        'SellPrice'            =>self::DATA_TYPE_DECIMAL
     );
-    protected static $primaryKey = 'ProductID';
-
+    
     public function getTableName(){
         return self::$tableName;
+    }
+    // Get the products with it's category
+    public static function getProducts(){
+        $sql = 'SELECT apl.*, apc.Name categoryName FROM '.self::$tableName. ' apl';
+        $sql .= ' INNER JOIN ' .CategoryModel::getModelTableName(). ' apc ON apl.CategoryID = apc.CategoryID';
+        return self::get($sql);
     }
 }
