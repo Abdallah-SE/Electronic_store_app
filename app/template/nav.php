bbaca48a7fb458b4f15e11c2fb4c625bd000a9f2
Abdallah-SE <abdalla.a911@yahoo.com>
2021-08-20 03:30:51 +0300

    initial commit

--- app/template/nav.php
+++ app/template/nav.php
@@ -25,7 +25,7 @@
                     </a>
                 </li>   
                 <li>
-                    <a href="/prodcuts"><i class="fa fa-shopping-bag"></i>
+                    <a href="/products"><i class="fa fa-shopping-bag"></i>
                         <?= @$text_products?>
                     </a>
                 </li>
